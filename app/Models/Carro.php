06ed57eb52d683bbb034a7266f7e4a401332b491
pedromartinello <pmartinello13@gmail.com>
2024-12-01 15:02:58 -0300

    Relacioamento

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Carro extends Model
 {
-    //
+    protected $fillable = [
+        'modelo', 
+        'ano', 
+        'pessoa_id'
+    ];
+
+    public function pessoa(): BelongsTo
+    {
+        return $this->belongsTo(Pessoa::class);
+    }
 }
+
--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pessoa extends Model
 {
-    //
+    protected $fillable = [
+        'nome', 
+        'idade'
+    ];
+
+    public function carros(): HasMany
+    {
+        return $this->hasMany(Carro::class);
+    }
 }
+
