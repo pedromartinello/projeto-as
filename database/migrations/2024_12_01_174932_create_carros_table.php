42b29f904eed2d908df321b31679bbd746cdac87
pedromartinello <pmartinello13@gmail.com>
2024-12-01 14:55:14 -0300

    Criação das models

---
+++ database/migrations/2024_12_01_174932_create_carros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->string('modelo');
+            $table->year('ano');
+            $table->unsignedBigInteger('pessoa_id');
+            $table->foreign('pessoa_id')->references('id')->on('pessoas')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carros');
+    }
+};
